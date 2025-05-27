0b20360aab9b16c98b710eb8fb3502d771a80d0e
victorchu <chutian0610@users.noreply.github.com>
2025-05-27 18:37:57 +0800

    Update MyResolvers.scala

--- project/MyResolvers.scala
+++ project/MyResolvers.scala
@@ -1,8 +1,8 @@
 import sbt.*
 
 object MyResolvers {
-  val aliyunPublic    = "Aliyun Public" at " https://maven.aliyun.com/repository/public"
-  val aliyunCenter  = "Aliyun Center" at "http://download.java.net/maven/glassfish"
+  val aliyunPublic    = "Aliyun Public" at "https://maven.aliyun.com/repository/public"
+  val aliyunCenter  = "Aliyun Center" at "https://maven.aliyun.com/repository/central"
   val huaweicloudMaven = "Huaweicloud Maven" at "https://repo.huaweicloud.com/repository/maven/"
   val mavenCentral = "Maven" at "https://repo1.maven.org/maven2"
 
